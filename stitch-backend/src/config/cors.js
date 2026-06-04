cec4061240bddba9ab3ab92e8d882494c16e651a
AK-GUPTA-20 <guptaakshat7795@gmail.com>
2026-06-04 22:59:15 +0530

    update cors policy

--- stitch-backend/src/config/cors.js
+++ stitch-backend/src/config/cors.js
@@ -3,6 +3,7 @@ const getCorsOptions = () => {
     process.env.FRONTEND_URL,
     "http://localhost:3000",
     "http://localhost:5173",
+    "https://stitch-clothing.vercel.app",
     
   ].filter(Boolean);
 
